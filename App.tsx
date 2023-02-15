2b50f03c61c133634ca50129e28b3374f5c855c3
Thibault Fischer <thibault@accountable.eu>
2023-02-15 11:31:56 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,3 +1,13 @@
-export const App = () => {
-  return <></>;
+import { Provider } from 'react-redux';
+import store from './src/store/store';
+import { Text } from 'react-native';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <></>
+    </Provider>
+  );
 };
+
+export default App;
---
+++ src/store/reducers/app.reducer.ts
@@ -0,0 +1,14 @@
+/* import {} from '../actions/app.actions'; */
+
+const initialState = {
+  showImages: true,
+};
+
+const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default appReducer;
